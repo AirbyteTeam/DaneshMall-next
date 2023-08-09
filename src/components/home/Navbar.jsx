3221ed5587246a853b115aacf8e2637c105c1508
Abolfazl <abolfazl30not30@gmail.com>
2023-08-09 21:26:50 +0330

    add navbar

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -3,22 +3,22 @@ import Link from "next/link"
 import Image from "next/image"
 import "../../styles/home/Navbar.css"
 import {NavLink} from "@/components/home/NavLink";
-import { useRouter } from "next/router";
+import { usePathname } from 'next/navigation'
 import {useEffect} from "react";
 
 function Navbar() {
-    const router = useRouter;
+    const pathname = usePathname()
     useEffect(()=>{
-        console.log(router.pathname)
+        console.log(pathname)
     },[])
     return (
         <nav className="bg-mainBg w-full h-[4.2rem] flex items-center justify-between px-10">
-                <div className="justify-end items-center">
+                <div className="items-center">
                     <ul className="flex gap-4">
-                        <li><Link  href="/"><span className={router.pathname === "/" ? "activeNavLink" : "text-white"} >خـانـه</span></Link></li>
-                        <li><Link  href="/about-us">دربـاره دانش مال</Link></li>
-                        <li><Link  href="/service">خدمـات</Link></li>
-                        <li><Link  href="/contact-us">تمـاس بـا مـا</Link></li>
+                        <li><Link  href="/"><span className={pathname === "/" ? "activeNavLink" : "text-white"} >خـانـه</span></Link></li>
+                        <li><Link  href="/about-us"><span className={pathname === "/about-us" ? "activeNavLink" : "text-white"} >دربـاره دانش مال</span></Link></li>
+                        <li><Link  href="/service"><span className={pathname === "/service" ? "activeNavLink" : "text-white"} >خـدمات</span></Link></li>
+                        <li><Link  href="/contact-us"><span className={pathname === "/contact-us" ? "activeNavLink" : "text-white"} >دربـاره ما</span></Link></li>
                     </ul>
                 </div>
                 <div className="w-[15%]">
