5d23dd0b0411aeb0b79d50534790f0872209554c
atta <attasamadi3@gmail.com>
2023-08-10 16:20:07 +0300

    About us page has been created

---
+++ src/app/about-us/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import AboutUS from '@/components/aboutUs_components/AboutUS'
+export default function AboutUs() {
+    return (
+        <div>
+            <AboutUS/>
+        </div>
+    )
+}
